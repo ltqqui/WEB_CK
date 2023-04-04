19ed2ce3cf87a79e8eb72c812c5cead889a1ff16
ltqqui <107937310+ltqqui@users.noreply.github.com>
2023-04-05 01:27:26 +0700

    xong respone

--- scroll.js
+++ scroll.js
@@ -1,10 +1,24 @@
 $(document).ready(function(){
     $(window).scroll(function(){
         if($(this).scrollTop()){
+            $('.fa-bars').addClass('text-light');
             $('.header').addClass('sticky');
+            $('.menu').addClass('sticky1');
+            $('.search-cart').addClass('sticky2');
         } 
-        else 
+        else {
+        $('.fa-bars').removeClass('text-light');
         $('.header').removeClass('sticky');
-   
+        $('.menu').removeClass('sticky1');
+        $('.search-cart').removeClass('sticky2');
+        }
+    })
+
+    $('.fa-bars').click(function(){
+        $('.menu').slideToggle();
+    })
+    $('.btn-account').click(function(){
+        $('.account-list').slideToggle();
     })
 })
+
